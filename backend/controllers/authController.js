2051d4216c4fd3297bec68847433a442cf57dd6b
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-02-19 23:40:58 +0530

    delete user auth updated

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -122,23 +122,16 @@ export const updateUser = asyncHandler(async(req, res) =>{
     })
 })
 
-export const deleteUser = async(req,res)=>{
-    try {
-        const userId = req.user.id
-        const user = await User.findById(userId)
-        
-        if (!user) {
-            return res.status(404).json({message : 'user not found'})
-        }
-        
-        // Delete all posts by this user
-        await Post.deleteMany({author: userId})
-        
-        // Delete the user
-        await User.findByIdAndDelete(userId)
-        
-        res.status(200).json({message : 'user account deleted successfully'})
-    } catch (error) {
-        res.status(500).json({message : 'internal server error'})
+export const deleteUser  = asyncHandler(async(req, res) => {
+    const userId = req.user.id
+    const deleted = await User.findByIdAndDelete(userId)
+    if(!deleted){
+        const error = new  Error('user not found')
+        error.statusCode = 404
+        throw error
     }
-}
+
+    await Post.deleteMany({author : userId}) //delete post also that are created by the user 
+    
+    res.status(200).json({success : true, message : 'user deleted successfully'})
+})
